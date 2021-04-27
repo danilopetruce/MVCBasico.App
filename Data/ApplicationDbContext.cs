d7ef03ebfe72e38a07cb4f1d287bd13bcc99541f
Danilo Petruce Marques <danilopetruce@hotmail.com>
2021-04-26 22:16:01 -0300

    Add project files.

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using MVCBasico.App.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MVCBasico.App.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Produto> Produtos { get; set; }
+        public DbSet<Fornecedor> Fornecedores { get; set; }
+        public DbSet<Endereco> Enderecos { get; set; }
+
+
+    }
+}
---
+++ Models/Endereco.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVCBasico.App.Models
+{
+    public class Endereco : Entity
+    {
+        public Guid FornecedorId { get; set; }
+
+        [Required(ErrorMessage = "O campo {0} é obrigatório!")]
+        [StringLength(200, ErrorMessage = "O Campo {0} precisa ter entre {2} e {1} caracteres", MinimumLength = 2)]
+        public String Logradouro { get; set; }
+
+        [Required(ErrorMessage = "O campo {0} é obrigatório!")]
+        [StringLength(50, ErrorMessage = "O Campo {0} precisa ter entre {2} e {1} caracteres", MinimumLength = 2)]
+        public String Numero { get; set; }
+
+        public String Complemento { get; set; }
+
+        [Required(ErrorMessage = "O campo {0} é obrigatório!")]
+        [StringLength(8, ErrorMessage = "O Campo {0} precisa ter entre {2} e {1} caracteres", MinimumLength = 8)]
+        public String Cep { get; set; }
+
+        [Required(ErrorMessage = "O campo {0} é obrigatório!")]
+        [StringLength(100, ErrorMessage = "O Campo {0} precisa ter entre {2} e {1} caracteres", MinimumLength = 2)]
+        public String Bairro { get; set; }
+
+        [Required(ErrorMessage = "O campo {0} é obrigatório!")]
+        [StringLength(100, ErrorMessage = "O Campo {0} precisa ter entre {2} e {1} caracteres", MinimumLength = 2)]
+        public String Cidade { get; set; }
+
+        [Required(ErrorMessage = "O campo {0} é obrigatório!")]
+        [StringLength(50, ErrorMessage = "O Campo {0} precisa ter entre {2} e {1} caracteres", MinimumLength = 2)]
+        public String Estado { get; set; }
+
+        /*EF RELATIONS */
+
+        public Fornecedor Fornecedor { get; set; }
+
+    }
+}
